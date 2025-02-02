f242a85c116a7aa2b85fc5b8414454d17083ccaa
Natasha <natiwhitney@gmail.com>
2025-02-02 16:17:31 -0500

    Update PriorityLRUCache.py
    Use a heap to keep track of minimum priority
    Delete items from heap lazily, at eviction time

--- PriorityLRUCache.py
+++ PriorityLRUCache.py
@@ -1,73 +1,55 @@
+import heapq
+
 class PQLRUCache:
   """Represents the cache, which is able to store and retrieve objects, as well as evict the PQ + LRU object from cache with evict() is called."""
 
   def __init__(self):
-    self.cache = [] # array of 4-length arrays, (k, v, p, access time-- counter)
-    self.key_dict = {} # map keys to their index (k, i)
-    self.prio_dict = {} # map prio+access time to their index (p+at, i)
+    self.cache = {} # (k -> (v, p, access time-- counter))
+    self.prio_heap = []  # will store (priority + usage, key) for easy min extraction
     self.counter = 0 # proxy for access time, an integer
-    
-  def put(self, k: str, v: str, p: int) -> None:
-    self.counter += 1 # increment access time
-    at = self.counter
-    obj = [k, v, p, at]
-    prio = self._prio_key(p, at)
-    # override existing object in cache
-    if k in self.key_dict:
-      i = self.key_dict[k]
-      ## 1) update prio_dict
-      _, _, p_prior, at_prior = self.cache[i]
-      prio_prior = self._prio_key(p_prior, at_prior)
-      del self.prio_dict[prio_prior]
-      self.prio_dict[prio] = i
-      ## 2) update cache
-      self.cache[i] = obj
-      ## 3) update key_dict -- N/A
-    # add new object to cache
-    else:
-      ## 1) update_prio dict
-      self.prio_dict[prio] = len(self.cache)
-      ## 2) append to cache
-      self.cache.append(obj)
-      ## 3) update key_dict
-      self.key_dict[k] = len(self.cache) - 1
-
 
   def _prio_key(self, p: int, at: int) -> str:
     return str(p) + ":" + str(at)
-    
+
+  def put(self, k: str, v: str, p: int) -> None:
+    # increment access time
+    self.counter += 1 
+    # update cache
+    self.cache[k] = (v, p, self.counter)
+    # push new prio to heap
+    prio_key = self._prio_key(p, self.counter)
+    heapq.heappush(self.prio_heap, (prio_key, k))
+
   def get(self, k: str) -> None:
     # if object not in cache, exit
-    if k not in self.key_dict:
+    if k not in self.cache:
       return None
 
-    # if obj in cache, update access time & override object in cache
+    # grab value, priority from cache
+    _, v, p, _ = self.cache[k]
+    # increment access time
     self.counter += 1
-    i = self.key_dict[k]
-    _, v, p, _ = self.cache[i]
-    self.cache[i] = [k, v, p, self.counter]
+    # udpate cache
+    self.cache[k] = (v, p, self.counter)
+    # push new prio to heap
+    prio_key = self._prio_key(p, self.counter)
+    heapq.heappush(self.prio_heap, (prio_key, k))
+
     # return value
     return v
 
   def evict(self) -> None:
-    # find the object(s) with the lowest priority, and then earliest access time
-    min_prio = min(self.prio_dict)
-    # grab its index
-    i = self.prio_dict[min_prio]
-    # grab its key from cache
-    k, _, _, _ = self.cache[i]
-    # delete its prio from min_prio
-    del self.prio_dict[min_prio]
-    # delete its object from cache
-    del self.cache[i]
-    # delete its index from key_dict
-    del self.key_dict[k]
+  # lazily evict min element from the heap, until up to date key is found
 
-    
-    
+    while self.prio_heap:
 
+      prio_key, k = heapq.heappop(self.prio_heap) # grab the min prio_key
 
-    
-    
-      
-     
+      if k not in self.cache: # already evicted, outdated
+        continue # go to next iteration of while loop
+
+      # see if the minimum is up to date in cache!
+      _, curr_p, curr_at = self.cache[k]
+      if self._prio_key(curr_p, curr_at) == prio_key:
+        del self.cache[k] # delete object from cache
+        break # exist while loop
